04eecf524867e84432e348c40e80f5ffeb220311
Rozghoniuk <nataliia.rozghoniuk@gmail.com>
2023-08-14 16:02:12 +0300

    ver 1.1 + adaptive

--- js.js
+++ js.js
@@ -188,10 +188,12 @@ movieBox.addEventListener("click", (ev) => {
   if (clickCard) {
     getDataById(API, KEY, clickCard.dataset.key);
     modal.style.display = "block";
+    document.body.style.overflow = "hidden";
 
   }
 })
 
 modalClose.addEventListener("click", () => { 
   modal.style.display = "none";
+  document.body.style.overflow = "";
 })
